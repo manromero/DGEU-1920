bf0ed4731ebd1215050bc422730fff4118332e29
Miguel Ángel Nuñez-Romero Olmo <miguelangel.nunez-romero.external@ts.fujitsu.com>
2020-08-30 16:15:55 +0200

    Breadcrumbs and tellafrined

--- dgeu/src/Body.tsx
+++ dgeu/src/Body.tsx
@@ -22,6 +22,8 @@ import ArticlesToDiscover from './ArticlesToDiscover';
 import RelatedProducts2 from './RelatedProducts2';
 import SpecialOffers from './SpecialOffers';
 import BoughtTogether from './BoughtTogether';
+import Breadcrumbs from './Breadcrumbs';
+import TellAFriend from './TellAFriend';
 
 const Body = () => {
   return (
@@ -38,20 +40,7 @@ const Body = () => {
         >
           <a href="https://www.amazon.es/Yo-Julia-Planeta-Volumen-independiente-ebook/dp/B07JK6ZS94"> y más de 8.000.000 libros están disponibles para <b>Amazon Kindle </b>. </a>
           <a href="https://www.amazon.es/Yo-Julia-Planeta-Volumen-independiente-ebook/dp/B07JK6ZS94">    </a></div><a href="https://www.amazon.es/Yo-Julia-Planeta-Volumen-independiente-ebook/dp/B07JK6ZS94"></a>
-        <div id="wayfinding-breadcrumbs_container" className="a-section a-spacing-none a-padding-medium"><a href="https://www.amazon.es/Yo-Julia-Planeta-Volumen-independiente-ebook/dp/B07JK6ZS94"></a>
-          <div id="wayfinding-breadcrumbs_feature_div" className="a-subheader a-breadcrumb feature" data-feature-name="wayfinding-breadcrumbs" data-cel-widget="wayfinding-breadcrumbs_feature_div"><a href="https://www.amazon.es/Yo-Julia-Planeta-Volumen-independiente-ebook/dp/B07JK6ZS94">
-          </a><ul className="a-unordered-list a-horizontal a-size-small"><a href="https://www.amazon.es/Yo-Julia-Planeta-Volumen-independiente-ebook/dp/B07JK6ZS94">
-            <li className="a-breadcrumb-divider"><span className="a-list-item a-color-tertiary">
-              ‹
-                </span></li>
-          </a><li><a href="https://www.amazon.es/Yo-Julia-Planeta-Volumen-independiente-ebook/dp/B07JK6ZS94"><span className="a-list-item">
-          </span></a><a id="breadcrumb-back-link" className="a-link-normal a-color-tertiary" href="https://www.amazon.es/s?k=yo+julia&amp;__mk_es_ES=%C3%85M%C3%85%C5%BD%C3%95%C3%91&amp;crid=2IKO067U954NA&amp;sprefix=yo+julia%2Caps%2C210&amp;ref=nb_sb_ss_i_1_8" >
-                  Volver a los resultados
-                    </a>
-              </li>
-            </ul>
-          </div>
-        </div>
+        <Breadcrumbs />
         <div id="dp-container" className="a-container" role="main">
           <div id="instantOrderUpdate_feature_div" className="celwidget" data-feature-name="instantOrderUpdate" data-csa-c-id="rmpp8r-388ol8-cbv4hj-jujkjm" data-cel-widget="instantOrderUpdate_feature_div">
             <div id="instantOrderUpdate_feature_div" data-feature-name="instantOrderUpdate" data-template-name="instantOrderUpdate" className="a-row feature" data-cel-widget="instantOrderUpdate_feature_div">
@@ -60,23 +49,7 @@ const Body = () => {
           <div id="companyCompliancePolicies_feature_div" className="celwidget" data-feature-name="companyCompliancePolicies" data-csa-c-id="amcusn-ks00f8-dnr8vx-nw3hye" data-cel-widget="companyCompliancePolicies_feature_div">
           </div>
           <div id="rightCol">
-            <div id="tellAFriendBylineBox_feature_div" className="celwidget" data-feature-name="tellAFriendBylineBox" data-csa-c-id="em3lqq-e7uh8k-6ug7jy-rzcwo1" data-cel-widget="tellAFriendBylineBox_feature_div">
-              <div className="celwidget" data-csa-c-id="3ak0ip-5ogkik-jbdepg-6y52em" data-cel-widget="tell-a-friend-byline">
-                <div id="tell-a-friend-byline" data-close="Cerrar" data-title="Compartir producto por Email" className="a-section a-text-center a-spacing-small">
-                  <div style={{ fontSize: "13px", lineHeight: "19px" }}>
-                    <a
-                      id="swfMailTo" className="a-link-normal email" title="Compartir por e-mail" href="mailto:?body=Quiero%20recomendar%20este%20producto%20en%20Amazon.com%0A%0AYo%2C%20Julia%3A%20Premio%20Planeta%202018%20(Autores%20Espa%C3%B1oles%20e%20Iberoamericanos)%0Apor%20Amazon%0AM%C3%A1s%20informaci%C3%B3n%3A%20https%3A%2F%2Fwww.amazon.es%2Fdp%2F8408197401%2Fref%3Dcm_sw_em_r_mt_dp_mv9rFbVVMD09P&amp;subject=Quiero%20recomendar%20este%20producto%20en%20Amazon">Compartir</a><span className="a-letter-space"></span>
-                    <a id="swfImageMailTo" className="a-link-normal email" title="Compartir por e-mail" href="mailto:?body=Quiero%20recomendar%20este%20producto%20en%20Amazon.com%0A%0AYo%2C%20Julia%3A%20Premio%20Planeta%202018%20(Autores%20Espa%C3%B1oles%20e%20Iberoamericanos)%0Apor%20Amazon%0AM%C3%A1s%20informaci%C3%B3n%3A%20https%3A%2F%2Fwww.amazon.es%2Fdp%2F8408197401%2Fref%3Dcm_sw_em_r_mt_dp_mv9rFbVVMD09P&amp;subject=Quiero%20recomendar%20este%20producto%20en%20Amazon"><i className="a-icon a-icon-share-email" role="img"></i></a><span className="a-letter-space"></span>
-                    <a data-height="400" className="a-link-normal swf-social-site" target="_blank" rel="noopener" title="Facebook" href="https://www.amazon.es/gp/redirect.html/ref=cm_sw_cl_fa_dp_mv9rFbVVMD09P?_encoding=UTF8&amp;location=https%3A%2F%2Fwww.facebook.com%2Fdialog%2Fshare%3F_encoding%3DUTF8%26app_id%3D465632727431967%26display%3Dpopup%26href%3Dhttps%253A%252F%252Fwww.amazon.es%252Fdp%252F8408197401%252Fref%253Dcm_sw_r_fa_dp_mv9rFbVVMD09P%26redirect_uri%3Dhttps%253A%252F%252Fwww.amazon.es%252Fgp%252Fpdp%252Ftaf%252Futility%252Ffb_post_redirect.html%252Fref%253Dcm_sw_s_fa_dp_mv9rFbVVMD09P%26hashtag%3D%2523Amazon&amp;token=03893D462A8A505D7C15ADE4DAFB7E48BECB13D5"><i className="a-icon a-icon-share-facebook" role="img" aria-label="Facebook"></i></a><span className="a-letter-space"></span>
-                    <a data-height="400" className="a-link-normal swf-social-site" target="_blank" rel="noopener" title="Twitter" href="https://www.amazon.es/gp/redirect.html/ref=cm_sw_cl_tw_dp_mv9rFbVVMD09P?_encoding=UTF8&amp;location=https%3A%2F%2Ftwitter.com%2Fintent%2Ftweet%3Foriginal_referer%3Dhttps%25253A%25252F%25252Fwww.amazon.es%25252Fgp%25252Fproduct%25252F8408197401%25252Fref%25253Dcm_sw_r_tw_dp_x_mv9rFbVVMD09P%26text%3DYo%252C%2520Julia%253A%2520Premio%2520Planeta%25202018%2520(Autores%2520Espa%25C3%25B1oles%2520e%2520Iberoamericanos)%2520de%2520Santi...%26related%3Damazondeals%252Camazonmp3%26url%3Dhttps%253A%252F%252Fwww.amazon.es%252Fdp%252F8408197401%252Fref%253Dcm_sw_r_tw_dp_x_mv9rFbVVMD09P%26via%3Damazon&amp;token=7E536E040F013F3A4B228AD42289A3670820D90E"><i className="a-icon a-icon-share-twitter" role="img" aria-label="Twitter"></i></a><span className="a-letter-space"></span>
-                    <a data-height="570" className="a-link-normal swf-social-site" target="_blank" rel="noopener" title="Pinterest" href="https://www.amazon.es/gp/redirect.html/ref=cm_sw_cl_pi_dp_mv9rFbVVMD09P?_encoding=UTF8&amp;location=https%3A%2F%2Fpinterest.com%2Fpin%2Fcreate%2Fbutton%3Furl%3Dhttps%253A%252F%252Fwww.amazon.es%252Fdp%252F8408197401%252Fref%253Dcm_sw_r_pi_dp_x_mv9rFbVVMD09P%26description%3DYo%252C%2520Julia%253A%2520Premio%2520Planeta%25202018%2520(Autores%2520Espa%25C3%25B1oles%2520e%2520Iberoamericanos)%2520de%2520Santiago%2520Posteguillo%26media%3Dhttps%253A%252F%252Fm.media-amazon.com%252Fimages%252FI%252F4191OJsSR1L.jpg&amp;token=C179F767BE1EB0DB5D70CA51CD25D37D1F6D2E56"><i className="a-icon a-icon-share-pinterest" role="img" aria-label="Pinterest"></i></a><span className="a-letter-space"></span>
-                    <span className="a-declarative" data-action="a-modal" data-a-modal="{&quot;padding&quot;:&quot;none&quot;,&quot;cache&quot;:&quot;false&quot;,&quot;width&quot;:&quot;832&quot;,&quot;name&quot;:&quot;kpModal&quot;,&quot;header&quot;:&quot;Comparte o incrusta una vista previa gratuita de un libro Kindle&quot;,&quot;popoverLabel&quot;:&quot;Comparte o incrusta una vista previa gratuita de un libro Kindle&quot;,&quot;url&quot;:&quot;https://leer.amazon.es/kp/taf/modal?asin=8408197401&amp;websiteDisplayGroup=book_display_on_website&amp;placementId=mv9rFbVVMD09P&amp;language=es_ES&quot;,&quot;height&quot;:&quot;664&quot;}">
-                      <a className="a-link-normal" title="Incrusta una visa previa de este libro" href="https://www.amazon.es/Yo-Julia-Planeta-Espa%C3%B1oles-Iberoamericanos/dp/8408197401/ref=sr_1_1?__mk_es_ES=%C3%85M%C3%85%C5%BD%C3%95%C3%91&amp;crid=2IKO067U954NA&amp;dchild=1&amp;keywords=yo+julia&amp;qid=1598543175&amp;sprefix=yo+julia%2Caps%2C210&amp;sr=8-1#">&lt;Incrustar&gt;</a>
-                    </span>
-                  </div>
-                </div>
-              </div>
-            </div>
+            <TellAFriend />
             <div id="pBookUpsell_feature_div" className="celwidget" data-feature-name="pBookUpsell" data-csa-c-id="bsv990-e7km14-f5pd9c-3ohm35" data-cel-widget="pBookUpsell_feature_div">
             </div>
             <BuyBoxFeature />
---
+++ dgeu/src/Breadcrumbs.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Breadcrumbs = () => {
+  return (
+    <div id="wayfinding-breadcrumbs_container" className="a-section a-spacing-none a-padding-medium">
+      <a href="https://www.amazon.es/Yo-Julia-Planeta-Volumen-independiente-ebook/dp/B07JK6ZS94"></a>
+      <div id="wayfinding-breadcrumbs_feature_div" className="a-subheader a-breadcrumb feature" data-feature-name="wayfinding-breadcrumbs" data-cel-widget="wayfinding-breadcrumbs_feature_div">
+        <a href="https://www.amazon.es/Yo-Julia-Planeta-Volumen-independiente-ebook/dp/B07JK6ZS94"></a>
+        <ul className="a-unordered-list a-horizontal a-size-small">
+          <a href="https://www.amazon.es/Yo-Julia-Planeta-Volumen-independiente-ebook/dp/B07JK6ZS94">
+            <li className="a-breadcrumb-divider">
+              <span className="a-list-item a-color-tertiary">
+                ‹
+              </span>
+            </li>
+          </a>
+          <li>
+            <a href="https://www.amazon.es/Yo-Julia-Planeta-Volumen-independiente-ebook/dp/B07JK6ZS94">
+              <span className="a-list-item"></span>
+            </a>
+            <a id="breadcrumb-back-link" className="a-link-normal a-color-tertiary" href="https://www.amazon.es/s?k=yo+julia&amp;__mk_es_ES=%C3%85M%C3%85%C5%BD%C3%95%C3%91&amp;crid=2IKO067U954NA&amp;sprefix=yo+julia%2Caps%2C210&amp;ref=nb_sb_ss_i_1_8" >
+              Volver a los resultados
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Breadcrumbs;
