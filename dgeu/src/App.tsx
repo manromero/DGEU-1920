05d23f870d369a3652ab74e466605e3472f196c4
Miguel Ángel Nuñez-Romero Olmo <miguelangel.nunez-romero.external@ts.fujitsu.com>
2020-08-30 18:05:33 +0200

    Footer

--- dgeu/src/App.tsx
+++ dgeu/src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './Header';
 import Body from './Body';
+import Footer from './Footer';
 
 const App = () => {
   return (
     <React.Fragment>
       <Header />
       <Body />
+      <Footer />
     </React.Fragment>
   );
 }
---
+++ dgeu/src/Footer.tsx
@@ -0,0 +1,258 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className="navLeftFooter nav-sprite-v1" id="navFooter">
+      <a href="https://www.amazon.es/Yo-Julia-Planeta-Espa%C3%B1oles-Iberoamericanos/dp/8408197401/ref=sr_1_1?__mk_es_ES=%C3%85M%C3%85%C5%BD%C3%95%C3%91&amp;crid=2IKO067U954NA&amp;dchild=1&amp;keywords=yo+julia&amp;qid=1598543175&amp;sprefix=yo+julia%2Caps%2C210&amp;sr=8-1#nav-top" id="navBackToTop">
+        <div className="navFooterBackToTop">
+          <span className="navFooterBackToTopText">Volver arriba</span>
+        </div>
+      </a>
+      <div className="navFooterVerticalColumn navAccessibility" role="presentation">
+        <div className="navFooterVerticalRow navAccessibility" style={{ display: "table-row" }}>
+          <div className="navFooterLinkCol navAccessibility">
+            <div className="navFooterColHead">Conócenos</div>
+            <ul>
+              <li className="nav_first">
+                <a href="https://www.amazon.es/b?ie=UTF8&amp;node=923374031&amp;ref_=footer_gw_m_b_careers" className="nav_a">Trabajar en Amazon</a>
+              </li>
+              <li>
+                <a href="http://www.amazon.es/sobreamazon" className="nav_a">Sobre Amazon.es</a>
+              </li>
+              <li className="nav_last">
+                <a href="https://sostenibilidad.aboutamazon.es/?utm_source=gateway&amp;utm_medium=footer&amp;ref_=susty_footer" className="nav_a">Sostenibilidad</a>
+              </li>
+            </ul>
+          </div>
+          <div className="navFooterColSpacerInner navAccessibility"></div>
+          <div className="navFooterLinkCol navAccessibility">
+            <div className="navFooterColHead">Gana dinero con nosotros</div>
+            <ul>
+              <li className="nav_first">
+                <a href="https://services.amazon.es/servicios/vender-por-internet/como-funciona-pro.html?ref=azessoafoo&amp;ld=AZESSOAFooter" className="nav_a">Vender en Amazon</a>
+              </li>
+              <li>
+                <a href="https://accelerator.amazon.es/?ref_=map_1_b2b_GW_FT" className="nav_a">Vender como Amazon Accelerator</a>
+              </li>
+              <li>
+                <a href="https://services.amazon.es/servicios/llegue-a-los-clientes-profesionales/caractersticas-y-ventajas.html?ld=AZESB2BRetailFooter" className="nav_a">Vender en Amazon Business</a>
+              </li>
+              <li>
+                <a href="https://services.amazon.es/handmade.htm?ref=azessoafoo&amp;ld=AZESHNDFooter" className="nav_a">Vende en Amazon Handmade</a>
+              </li>
+              <li>
+                <a href="https://afiliados.amazon.es/" className="nav_a">Programa de afiliados</a>
+              </li>
+              <li>
+                <a href="https://services.amazon.es/servicios/logistica-de-amazon/funciones-y-ventajas/?ld=AZESFBAFooter" className="nav_a">Logística de Amazon</a>
+              </li>
+              <li>
+                <a href="https://advertising.amazon.es/products-self-serve?ref_=ext_amzn_ftr" className="nav_a">Promociona tus productos</a>
+              </li>
+              <li>
+                <a href="https://kdp.amazon.com/?language=es_ES" className="nav_a">Publica tu libro en Kindle</a>
+              </li>
+              <li className="nav_last">
+                <a href="https://pay.amazon.com/es?ld=AWREESAPAFooter" className="nav_a">Amazon Pay</a>
+              </li>
+            </ul>
+          </div>
+          <div className="navFooterColSpacerInner navAccessibility"></div>
+          <div className="navFooterLinkCol navAccessibility">
+            <div className="navFooterColHead">Métodos de pago Amazon</div>
+            <ul>
+              <li className="nav_first">
+                <a href="https://www.amazon.es/gp/help/customer/display.html?ie=UTF8&amp;nodeId=201262600&amp;ref_=footer_payment" className="nav_a">Métodos de pago</a>
+              </li>
+              <li>
+                <a href="https://www.amazon.es/gp/help/customer/display.html?ie=UTF8&amp;nodeId=201239250&amp;ref_=footer_tfx" className="nav_a">Conversor de divisas de Amazon</a>
+              </li>
+              <li>
+                <a href="https://www.amazon.es/gp/gc?ie=UTF8&amp;ref_=footer_giftcards" className="nav_a">Cheques Regalo</a></li><li><a href="https://www.amazon.es/gp/gc/create?ie=UTF8&amp;ref_=footer_topup_es" className="nav_a">Recarga online</a>
+              </li>
+              <li className="nav_last">
+                <a href="https://www.amazon.es/b?ie=UTF8&amp;node=15417070031&amp;ref_=footer_purchase_code" className="nav_a">Recarga en tienda</a>
+              </li>
+            </ul>
+          </div>
+          <div className="navFooterColSpacerInner navAccessibility"></div>
+          <div className="navFooterLinkCol navAccessibility">
+            <div className="navFooterColHead">¿Necesitas ayuda?</div>
+            <ul>
+              <li className="nav_first">
+                <a href="https://www.amazon.es/gp/help/customer/display.html?ie=UTF8&amp;nodeId=GDFU3JS5AL6SYHRD&amp;ref_=footer_covid" className="nav_a">Amazon y COVID-19</a>
+              </li>
+              <li>
+                <a href="https://www.amazon.es/gp/css/order-history?ie=UTF8&amp;ref_=footer_hp_ss_comp_tmp" className="nav_a">Localizar o gestionar compras</a>
+              </li>
+              <li>
+                <a href="https://www.amazon.es/gp/help/customer/display.html?ie=UTF8&amp;nodeId=200522630&amp;ref_=footer_shiprates" className="nav_a">Tarifas y políticas de envío</a>
+              </li>
+              <li>
+                <a href="https://www.amazon.es/gp/prime?ie=UTF8&amp;ref_=footer_prime" className="nav_a">Amazon Prime</a>
+              </li>
+              <li>
+                <a href="https://www.amazon.es/gp/css/returns/homepage.html?ie=UTF8&amp;ref_=footer_hy_f_4" className="nav_a">Devolver o reemplazar productos</a>
+              </li>
+              <li>
+                <a href="https://www.amazon.es/gp/help/customer/display.html?ie=UTF8&amp;nodeId=201819410&amp;ref_=footer_disposal" className="nav_a">Reciclaje</a>
+              </li>
+              <li>
+                <a href="https://www.amazon.es/hz/mycd/myx?_encoding=UTF8&amp;ref_=footer_myk" className="nav_a">Gestionar contenido y dispositivos</a>
+              </li>
+              <li>
+                <a href="https://www.amazon.es/b?ie=UTF8&amp;node=5061838031&amp;ref_=footer_mobapp" className="nav_a">App Amazon</a>
+              </li>
+              <li>
+                <a href="https://www.amazon.es/gp/BIT/ref=footer_bit_v2_e0005?bitCampaignCode=e0005" className="nav_a">Amazon Assistant</a>
+              </li>
+              <li>
+                <a href="https://www.amazon.es/gp/help/customer/display.html?ie=UTF8&amp;nodeId=200507590&amp;ref_=footer_gw_m_b_cs" className="nav_a">Atención al Cliente</a>
+              </li>
+              <li className="nav_last">
+                <a href="https://www.amazon.es/gp/help/customer/display.html?ie=UTF8&amp;nodeId=201239260&amp;ref_=footer_vat" className="nav_a">IVA sobre los bienes</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className="nav-footer-line"></div>
+      <div className="navFooterLine navFooterLinkLine navFooterPadItemLine">
+        <span>
+          <div className="navFooterLine navFooterLogoLine">
+            <a href="https://www.amazon.es/ref=footer_logo">
+              <div className="nav-logo-base nav-sprite"></div>
+            </a>
+          </div>
+        </span>
+        <ul></ul>
+      </div>
+      <div className="navFooterLine navFooterLinkLine navFooterPadItemLine ">
+        <ul>
+          <li className="nav_first">
+            <a href="https://www.amazon.com.au/ref=footer_au" className="nav_a">Australia</a>
+          </li>
+          <li>
+            <a href="https://www.amazon.de/ref=footer_de" className="nav_a">Alemania</a>
+          </li>
+          <li>
+            <a href="https://www.amazon.com.br/ref=footer_br" className="nav_a">Brasil</a>
+          </li>
+          <li>
+            <a href="https://www.amazon.ca/ref=footer_ca" className="nav_a">Canadá</a>
+          </li>
+          <li>
+            <a href="https://www.amazon.cn/ref=footer_cn" className="nav_a">China</a>
+          </li>
+          <li>
+            <a href="https://www.amazon.com/ref=footer_us" className="nav_a">Estados Unidos</a>
+          </li>
+          <li>
+            <a href="https://www.amazon.fr/ref=footer_fr" className="nav_a">Francia</a>
+          </li>
+          <li>
+            <a href="https://www.amazon.in/ref=footer_in" className="nav_a">India</a>
+          </li>
+          <li>
+            <a href="https://www.amazon.it/ref=footer_it" className="nav_a">Italia</a>
+          </li>
+          <li>
+            <a href="https://www.amazon.co.jp/ref=footer_jp" className="nav_a">Japón</a>
+          </li>
+          <li>
+            <a href="https://www.amazon.com.mx/ref=footer_mx" className="nav_a">México</a>
+          </li>
+          <li>
+            <a href="https://www.amazon.nl/ref=footer_nl" className="nav_a">Países Bajos</a>
+          </li>
+          <li>
+            <a href="https://www.amazon.ae/ref=footer_ae" className="nav_a">Emiratos Árabes Unidos</a>
+          </li>
+          <li>
+            <a href="https://www.amazon.co.uk/ref=footer_uk" className="nav_a">Reino Unido</a>
+          </li>
+          <li>
+            <a href="https://www.amazon.sg/ref=footer_sg" className="nav_a">Singapur</a>
+          </li>
+          <li className="nav_last">
+            <a href="https://www.amazon.com.tr/ref=footer_tr" className="nav_a">Turquía</a>
+          </li>
+        </ul>
+      </div>
+      <div className="navFooterLine navFooterLinkLine navFooterDescLine">
+        <table className="navFooterMoreOnAmazon">
+          <tbody>
+            <tr>
+              <td className="navFooterDescItem">
+                <a href="https://music.amazon.es/?ref=dm_aff_amz_es" className="nav_a">Amazon Music<br /> <span className="navFooterDescText">Transmite millones<br /> de canciones</span></a>
+              </td>
+              <td className="navFooterDescSpacer" style={{ width: "4%" }}></td>
+              <td className="navFooterDescItem">
+                <a href="https://www.iberlibro.com/" className="nav_a">IberLibro<br /> <span className="navFooterDescText">Libros, arte<br /> &amp; artículos de colección</span></a>
+              </td>
+              <td className="navFooterDescSpacer" style={{ width: "4%" }}></td>
+              <td className="navFooterDescItem">
+                <a href="https://aws.amazon.com/es/?sc_channel=EL&amp;sc_campaign=ES_amazonfooter" className="nav_a">Amazon Web Services<br /> <span className="navFooterDescText">Servicios Cloud Computing<br /> de Amazon</span></a>
+              </td>
+              <td className="navFooterDescSpacer" style={{ width: "4%" }}></td>
+              <td className="navFooterDescItem">
+                <a href="https://www.bookdepository.com/" className="nav_a">Book Depository<br /> <span className="navFooterDescText">Libros con entrega gratis<br /> en todo el mundo</span></a>
+              </td>
+              <td className="navFooterDescSpacer" style={{ width: "4%" }}></td>
+              <td className="navFooterDescItem">
+                <a href="https://kdp.amazon.com/?language=es_ES" className="nav_a">Kindle Direct Publishing<br /> <span className="navFooterDescText">Publica tu libro en papel y digital<br /> de manera independiente</span></a>
+              </td>
+            </tr>
+            <tr>
+              <td>&nbsp;</td>
+            </tr>
+            <tr>
+              <td className="navFooterDescItem">
+                <a href="https://primenow.amazon.es/?ref=HOUD12C322_0_GlobalFooter" className="nav_a">Prime Now<br /> <span className="navFooterDescText">Entrega en 1 hora<br /> En miles de productos</span></a>
+              </td>
+              <td className="navFooterDescSpacer" style={{ width: "4%" }}></td>
+              <td className="navFooterDescItem">
+                <a href="https://www.amazon.es/Productos-Reacondicionados/b?ie=UTF8&amp;node=3582001031&amp;ref_=footer_wrhsdls" className="nav_a">Productos Reacondicionados<br /> <span className="navFooterDescText">Precios bajos en productos revisados por Amazon</span></a>
+              </td>
+              <td className="navFooterDescSpacer" style={{ width: "4%" }}></td>
+              <td className="navFooterDescItem">
+                <a href="https://www.shopbop.com/" className="nav_a">Shopbop<br /> <span className="navFooterDescText">Marcas<br /> de moda de diseño </span></a>
+              </td>
+              <td className="navFooterDescSpacer" style={{ width: "4%" }}></td>
+              <td className="navFooterDescItem">
+                <a href="https://www.amazon.es/b?ie=UTF8&amp;node=14862331031&amp;ref_=nav_footer_business" className="nav_a">Amazon Business<br /> <span className="navFooterDescText">Servicio para<br /> clientes de empresa</span></a>
+              </td>
+              <td className="navFooterDescSpacer" style={{ width: "4%" }}></td>
+              <td className="navFooterDescItem">
+                <a href="https://www.amazon.es/amazonsecondchance?_encoding=UTF8&amp;ref_=footer_asc" className="nav_a">Amazon Second Chance<br /> <span className="navFooterDescText">Donar, intercambiar,<br /> dar una segunda vida</span></a>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div className="navFooterLine navFooterLinkLine navFooterPadItemLine navFooterCopyright">
+        <ul>
+          <li className="nav_first">
+            <a href="https://www.amazon.es/gp/help/customer/display.html?ie=UTF8&amp;nodeId=201909000&amp;ref_=footer_cou" className="nav_a">Condiciones de Uso y Venta</a>
+          </li>
+          <li>
+            <a href="https://www.amazon.es/gp/help/customer/display.html?ie=UTF8&amp;nodeId=201909010&amp;ref_=footer_privacy" className="nav_a">Aviso de privacidad</a>
+          </li>
+          <li>
+            <a href="https://www.amazon.es/gp/help/customer/display.html?ie=UTF8&amp;nodeId=201909250&amp;ref_=footer_legal_area" className="nav_a">Área legal</a>
+          </li>
+          <li>
+            <a href="https://www.amazon.es/gp/help/customer/display.html?ie=UTF8&amp;nodeId=201890250&amp;ref_=footer_cookies_notice" className="nav_a">Cookies</a>
+          </li>
+          <li className="nav_last">
+            <a href="https://www.amazon.es/gp/help/customer/display.html?ie=UTF8&amp;nodeId=201909150&amp;ref_=footer_Interest_Based_Ads_Notice" className="nav_a">Publicidad basada en intereses</a>
+          </li>
+        </ul>
+        <span>© 1996-2020, Amazon.com, Inc. o afiliados. Todos los derechos reservados.</span>
+        <ul></ul>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
