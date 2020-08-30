70f14f0f6fb21c045fc11b82351424ccf8097e3d
Miguel Ángel Nuñez-Romero Olmo <miguelangel.nunez-romero.external@ts.fujitsu.com>
2020-08-30 14:14:53 +0200

    BoughtTogether

---
+++ dgeu/src/BoughtTogether.tsx
@@ -0,0 +1,202 @@
+import React from 'react';
+
+const BoughtTogether = () => {
+  return (
+    <div className="celwidget pd_rd_w-yDUK0 pf_rd_p-12d945b9-5148-4c92-8c2a-6d4b36ee4de4 pf_rd_r-VBVEMCD60R9FPEJFGNHW pd_rd_r-c6b0e583-be80-4aa3-867d-b89df4127853 pd_rd_wg-nVWqQ" data-csa-c-id="68ggv9-b78gdf-jwwkc5-h187yz" data-cel-widget="desktop-dp-sims_fbt">
+      <div id="sims-fbt" className="similarities-widget bucket">
+        <hr className="a-divider-normal bucketDivider" />
+        <h2>Comprados juntos habitualmente</h2>
+        <div id="sims-fbt-container" data-p13n-feature-metadata="{&quot;baseAsin&quot;:&quot;8408197401&quot;,&quot;has_diff_avail&quot;:&quot;0&quot;,&quot;has_seller_difference&quot;:&quot;0&quot;,&quot;pd_rd_wg&quot;:&quot;nVWqQ&quot;,&quot;pd_rd_r&quot;:&quot;c6b0e583-be80-4aa3-867d-b89df4127853&quot;,&quot;pf_rd_r&quot;:&quot;VBVEMCD60R9FPEJFGNHW&quot;,&quot;pf_rd_p&quot;:&quot;12d945b9-5148-4c92-8c2a-6d4b36ee4de4&quot;,&quot;pd_rd_w&quot;:&quot;yDUK0&quot;}" data-p13n-feature-name="pd_bxgy" data-p13n-global="{&quot;customerId&quot;:0,&quot;marketplace&quot;:&quot;ES&quot;,&quot;requestId&quot;:&quot;VBVEMCD60R9FPEJFGNHW&quot;,&quot;marketplaceId&quot;:&quot;A1RKKUPIHCS9HS&quot;,&quot;session&quot;:&quot;259-6544150-3296161&quot;}" className="a-container">
+          <div id="sims-fbt-content">
+            <form id="sims-fbt-form" name="BuyXGetYhandleBuy" method="post" action="https://www.amazon.es/gp/item-dispatch/ref=pd_bxgy_button_crt/259-6544150-3296161?ie=UTF8&amp;pd_rd_r=c6b0e583-be80-4aa3-867d-b89df4127853&amp;pd_rd_w=yDUK0&amp;pd_rd_wg=nVWqQ&amp;pf_rd_p=12d945b9-5148-4c92-8c2a-6d4b36ee4de4&amp;pf_rd_r=VBVEMCD60R9FPEJFGNHW&amp;refRID=VBVEMCD60R9FPEJFGNHW" >
+              <input type="hidden" name="session-id" value="259-6544150-3296161" />
+              <input type="hidden" name="discoveredAsins.1" value="8408224697" />
+              <input type="hidden" name="discoveredAsins.2" value="8420435473" />
+              <div>
+                <ul className="a-unordered-list a-nostyle a-horizontal a-spacing-base a-spacing-top-base sims-fbt-image-box">
+                  <li className="a-align-center sims-fbt-image-1">
+                    <span className="a-list-item">
+                      <div className="a-section">
+                        <img alt="Yo, Julia: Premio Planeta 2018 (Autores Españoles e Iberoamericanos)" src="./Yo, Julia_ Premio Planeta 2018 Autores Españoles e Iberoamericanos_ Amazon.es_ Posteguillo, Santiago_ Libros_files/91vOlX49DIL._AC_UL115_.jpg" className="sims-fbt-image" />
+                      </div>
+                    </span>
+                  </li>
+                  <li className="a-align-center sims-fbt-plus-2">
+                    <span className="a-list-item a-size-large a-color-tertiary">+</span>
+                  </li>
+                  <li className="a-align-center sims-fbt-image-2">
+                    <span className="a-list-item">
+                      <a className="a-link-normal" href="https://www.amazon.es/Julia-dioses-Autores-Espa%C3%B1oles-Iberoamericanos/dp/8408224697/ref=pd_bxgy_img_2/259-6544150-3296161?_encoding=UTF8&amp;pd_rd_i=8408224697&amp;pd_rd_r=c6b0e583-be80-4aa3-867d-b89df4127853&amp;pd_rd_w=yDUK0&amp;pd_rd_wg=nVWqQ&amp;pf_rd_p=12d945b9-5148-4c92-8c2a-6d4b36ee4de4&amp;pf_rd_r=VBVEMCD60R9FPEJFGNHW&amp;psc=1&amp;refRID=VBVEMCD60R9FPEJFGNHW">
+                        <div className="a-section">
+                          <img alt="Y Julia retó a los dioses: Cuando el enemigo es tu propio hijo..., ¿existe la victoria? (Autores Españoles e Iberoamericanos)" src="./Yo, Julia_ Premio Planeta 2018 Autores Españoles e Iberoamericanos_ Amazon.es_ Posteguillo, Santiago_ Libros_files/911XSy2BH9L._AC_UL115_.jpg" className="sims-fbt-image" />
+                        </div>
+                      </a>
+                    </span>
+                  </li>
+                  <li className="a-align-center sims-fbt-plus-3">
+                    <span className="a-list-item a-size-large a-color-tertiary">+</span>
+                  </li>
+                  <li className="a-align-center sims-fbt-image-3">
+                    <span className="a-list-item">
+                      <a className="a-link-normal" href="https://www.amazon.es/Sidi-HISPANICA-Arturo-P%C3%A9rez-Reverte/dp/8420435473/ref=pd_bxgy_img_3/259-6544150-3296161?_encoding=UTF8&amp;pd_rd_i=8420435473&amp;pd_rd_r=c6b0e583-be80-4aa3-867d-b89df4127853&amp;pd_rd_w=yDUK0&amp;pd_rd_wg=nVWqQ&amp;pf_rd_p=12d945b9-5148-4c92-8c2a-6d4b36ee4de4&amp;pf_rd_r=VBVEMCD60R9FPEJFGNHW&amp;psc=1&amp;refRID=VBVEMCD60R9FPEJFGNHW">
+                        <div className="a-section">
+                          <img alt="Sidi (Hispánica)" src="./Yo, Julia_ Premio Planeta 2018 Autores Españoles e Iberoamericanos_ Amazon.es_ Posteguillo, Santiago_ Libros_files/71N6zowIrIL._AC_UL115_.jpg" className="sims-fbt-image" />
+                        </div>
+                      </a>
+                    </span>
+                  </li>
+                </ul>
+                <div className="sims-fbt-price-box">
+                  <div className="sims-fbt-total-price">
+                    <span className="a-size-base a-color-secondary sims-fbt-total-price-text">Precio total: </span>
+                    <span className="a-size-medium a-color-price sims-fbt-total-price-value">
+                      <span className="p13n-sc-price">63,35&nbsp;€</span>
+                    </span>
+                  </div>
+                  <div className="sims-fbt-buttons">
+                    <div className="a-button-stack a-spacing-mini">
+                      <span className="a-button a-button-primary a-button-small sims-fbt-add-to-cart" id="a-autoid-14">
+                        <span className="a-button-inner">
+                          <input name="submit.addToCart" className="a-button-input" type="submit" aria-labelledby="a-autoid-14-announce" />
+                          <span className="a-button-text" aria-hidden="true" id="a-autoid-14-announce">Añadir los tres a la cesta</span>
+                        </span>
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="sims-fbt-rows">
+                <fieldset className="forScreenreaders">
+                  <legend>
+                    <span>Comprar los productos seleccionados conjuntamente</span>
+                  </legend>
+                  <ul className="a-unordered-list a-nostyle a-vertical">
+                    <li data-p13n-asin-metadata="{&quot;ref&quot;:&quot;pd_bxgy_1_1&quot;,&quot;price&quot;:21.75,&quot;asin&quot;:&quot;8408197401&quot;}" className="a-spacing-mini p13n-asin">
+                      <span className="a-list-item">
+                        <span className="a-declarative" data-action="fbt-checkbox-click" data-fbt-checkbox-click="{&quot;itemNum&quot;:&quot;1&quot;,&quot;offerListingID&quot;:&quot;iPonKd4%2BzUM3asnsIRdpeoB%2Bu%2BQhwVpCLSJhVDRTWYvrXcsBjObhqrddQq%2FL4HuRDKdryn%2BW9h2moTXk3yRlexGLYHbpMjceX58ArLCenbo8EzZI%2Bbm9JZ5JvpHVl7pRgd3sEou5i%2Fv42qtRhr6q8w%3D%3D&quot;,&quot;asin&quot;:&quot;8408197401&quot;}">
+                          <div data-a-input-name="offeringID.1" className="a-checkbox sims-fbt-checkbox">
+                            <label>
+                              <input type="checkbox" name="offeringID.1" value="iPonKd4%2BzUM3asnsIRdpeoB%2Bu%2BQhwVpCLSJhVDRTWYvrXcsBjObhqrddQq%2FL4HuRDKdryn%2BW9h2moTXk3yRlexGLYHbpMjceX58ArLCenbo8EzZI%2Bbm9JZ5JvpHVl7pRgd3sEou5i%2Fv42qtRhr6q8w%3D%3D" style={{ display: "inline-block" }} />
+                              <i className="a-icon a-icon-checkbox"></i>
+                              <span className="a-label a-checkbox-label">
+                                <div className="sims-fbt-checkbox-label">
+                                  <span className="sims-fbt-this-item a-text-bold">Este producto:</span>
+                                  <span>Yo, Julia: Premio Planeta 2018 (Autores Españoles e Iberoamericanos)</span>
+                                  <span className="a-size-small"> por Santiago Posteguillo</span>
+                                  <span className="a-size-small a-color-secondary"> Tapa dura</span>
+                                  <span className="a-color-price">
+                                    <span className="p13n-sc-price">21,75&nbsp;€</span>
+                                  </span>
+                                  <div className="a-row a-spacing-micro a-spacing-top-micro a-size-small a-color-secondary sims-fbt-seller-info">
+                                    <div className="a-row">
+                                      <span className="a-size-base a-color-success">En stock.</span>
+                                    </div>
+                                    <div className="a-row">
+                                      <span className="a-size-base a-color-secondary a-text-normal">Envíos desde y vendidos por Amazon.</span>
+                                    </div>
+                                    <div className="a-row">
+                                      <span className="a-size-base a-color-secondary a-text-normal">
+                                        Está disponible el
+                                        <b>envío GRATIS</b>
+                                        para este producto.
+                                        <a className="a-link-normal sims-fbt-details-help-link a-declarative" target="AmazonHelp" rel="noopener" href="https://www.amazon.es/gp/help/customer/display.html/ref=pd_bxgy_1_sdc_sdc_1/259-6544150-3296161?ie=UTF8&amp;nodeId=201911090&amp;pd_rd_r=c6b0e583-be80-4aa3-867d-b89df4127853&amp;pd_rd_w=yDUK0&amp;pd_rd_wg=nVWqQ&amp;pf_rd_p=12d945b9-5148-4c92-8c2a-6d4b36ee4de4&amp;pf_rd_r=VBVEMCD60R9FPEJFGNHW&amp;pop-up=1&amp;refRID=VBVEMCD60R9FPEJFGNHW">Detalles</a>
+                                      </span>
+                                    </div>
+                                  </div>
+                                </div>
+                              </span>
+                            </label>
+                          </div>
+                        </span>
+                      </span>
+                    </li>
+                    <li data-p13n-asin-metadata="{&quot;ref&quot;:&quot;pd_bxgy_2_2&quot;,&quot;price&quot;:21.75,&quot;asin&quot;:&quot;8408224697&quot;}" className="a-spacing-mini p13n-asin">
+                      <span className="a-list-item">
+                        <span className="a-declarative" data-action="fbt-checkbox-click" data-fbt-checkbox-click="{&quot;itemNum&quot;:&quot;2&quot;,&quot;offerListingID&quot;:&quot;uPDDYrn5Z%2BcP1KrWcoyaMmuGwesqxMbAi%2FAwrAeKDho%2BjjvzSPwMKBdoifA8N%2BwBYbLg%2FYHjRupSx78eaqJXX%2BrZ4ezjyKbDmSlbtE9xKaTnk69ZrC7%2BnqgCYrwJbUwm4vlMcrmaGP2An4%2BEFPNKwQ%3D%3D&quot;,&quot;asin&quot;:&quot;8408224697&quot;}">
+                          <div data-a-input-name="offeringID.2" className="a-checkbox sims-fbt-checkbox">
+                            <label>
+                              <input type="checkbox" name="offeringID.2" value="uPDDYrn5Z%2BcP1KrWcoyaMmuGwesqxMbAi%2FAwrAeKDho%2BjjvzSPwMKBdoifA8N%2BwBYbLg%2FYHjRupSx78eaqJXX%2BrZ4ezjyKbDmSlbtE9xKaTnk69ZrC7%2BnqgCYrwJbUwm4vlMcrmaGP2An4%2BEFPNKwQ%3D%3D" style={{ display: "inline-block" }} />
+                              <i className="a-icon a-icon-checkbox"></i>
+                              <span className="a-label a-checkbox-label">
+                                <div className="sims-fbt-checkbox-label">
+                                  <a className="a-size-base a-link-normal" href="https://www.amazon.es/Julia-dioses-Autores-Espa%C3%B1oles-Iberoamericanos/dp/8408224697/ref=pd_bxgy_2/259-6544150-3296161?_encoding=UTF8&amp;pd_rd_i=8408224697&amp;pd_rd_r=c6b0e583-be80-4aa3-867d-b89df4127853&amp;pd_rd_w=yDUK0&amp;pd_rd_wg=nVWqQ&amp;pf_rd_p=12d945b9-5148-4c92-8c2a-6d4b36ee4de4&amp;pf_rd_r=VBVEMCD60R9FPEJFGNHW&amp;psc=1&amp;refRID=VBVEMCD60R9FPEJFGNHW">
+                                    Y Julia retó a los dioses: Cuando el enemigo es tu propio hijo..., ¿existe la victoria? (Autores…
+                                    </a>
+                                  <span className="a-size-small"> por Santiago Posteguillo</span>
+                                  <span className="a-size-small a-color-secondary"> Tapa dura</span>
+                                  <span className="a-color-price">
+                                    <span className="p13n-sc-price">21,75&nbsp;€</span>
+                                  </span>
+                                  <div className="a-row a-spacing-micro a-spacing-top-micro a-size-small a-color-secondary sims-fbt-seller-info">
+                                    <div className="a-row">
+                                      <span className="a-size-base a-color-success">En stock.</span>
+                                    </div>
+                                    <div className="a-row">
+                                      <span className="a-size-base a-color-secondary a-text-normal">Envíos desde y vendidos por Amazon.</span>
+                                    </div>
+                                    <div className="a-row">
+                                      <span className="a-size-base a-color-secondary a-text-normal">
+                                        Está disponible el
+                                          <b>envío GRATIS</b>
+                                          para este producto.
+                                          <a className="a-link-normal sims-fbt-details-help-link a-declarative" target="AmazonHelp" rel="noopener" href="https://www.amazon.es/gp/help/customer/display.html/ref=pd_bxgy_2_sdc_sdc_2/259-6544150-3296161?ie=UTF8&amp;nodeId=201911090&amp;pd_rd_r=c6b0e583-be80-4aa3-867d-b89df4127853&amp;pd_rd_w=yDUK0&amp;pd_rd_wg=nVWqQ&amp;pf_rd_p=12d945b9-5148-4c92-8c2a-6d4b36ee4de4&amp;pf_rd_r=VBVEMCD60R9FPEJFGNHW&amp;pop-up=1&amp;refRID=VBVEMCD60R9FPEJFGNHW">Detalles</a>
+                                      </span>
+                                    </div>
+                                  </div>
+                                </div>
+                              </span>
+                            </label>
+                          </div>
+                        </span>
+                      </span>
+                    </li>
+                    <li data-p13n-asin-metadata="{&quot;ref&quot;:&quot;pd_bxgy_3_3&quot;,&quot;price&quot;:19.85,&quot;asin&quot;:&quot;8420435473&quot;}" className="a-spacing-mini p13n-asin">
+                      <span className="a-list-item">
+                        <span className="a-declarative" data-action="fbt-checkbox-click" data-fbt-checkbox-click="{&quot;itemNum&quot;:&quot;3&quot;,&quot;offerListingID&quot;:&quot;VqJ6wqiYMHxFUDWDd1EYC6xF9fCp3vrEEHIxT5j9Bvggr05K1Ia0Wj%2BEvdvsz1oLmn5jboaqDXU2kBf1xGAFwe4saRUj3JytS6m7rDZpoDrcTwpgACZnygaNYbctdIb061TXicCFfDksayAety1QRg%3D%3D&quot;,&quot;asin&quot;:&quot;8420435473&quot;}">
+                          <div data-a-input-name="offeringID.3" className="a-checkbox sims-fbt-checkbox">
+                            <label>
+                              <input type="checkbox" name="offeringID.3" value="VqJ6wqiYMHxFUDWDd1EYC6xF9fCp3vrEEHIxT5j9Bvggr05K1Ia0Wj%2BEvdvsz1oLmn5jboaqDXU2kBf1xGAFwe4saRUj3JytS6m7rDZpoDrcTwpgACZnygaNYbctdIb061TXicCFfDksayAety1QRg%3D%3D" style={{ display: "inline-block" }} />
+                              <i className="a-icon a-icon-checkbox"></i>
+                              <span className="a-label a-checkbox-label">
+                                <div className="sims-fbt-checkbox-label">
+                                  <a className="a-size-base a-link-normal" href="https://www.amazon.es/Sidi-HISPANICA-Arturo-P%C3%A9rez-Reverte/dp/8420435473/ref=pd_bxgy_3/259-6544150-3296161?_encoding=UTF8&amp;pd_rd_i=8420435473&amp;pd_rd_r=c6b0e583-be80-4aa3-867d-b89df4127853&amp;pd_rd_w=yDUK0&amp;pd_rd_wg=nVWqQ&amp;pf_rd_p=12d945b9-5148-4c92-8c2a-6d4b36ee4de4&amp;pf_rd_r=VBVEMCD60R9FPEJFGNHW&amp;psc=1&amp;refRID=VBVEMCD60R9FPEJFGNHW">Sidi (Hispánica)</a>
+                                  <span className="a-size-small"> por Arturo Pérez-Reverte</span>
+                                  <span className="a-size-small a-color-secondary"> Tapa dura</span>
+                                  <span className="a-color-price">
+                                    <span className="p13n-sc-price">19,85&nbsp;€</span>
+                                  </span>
+                                  <div className="a-row a-spacing-micro a-spacing-top-micro a-size-small a-color-secondary sims-fbt-seller-info">
+                                    <div className="a-row">
+                                      <span className="a-size-base a-color-success">En stock.</span>
+                                    </div>
+                                    <div className="a-row">
+                                      <span className="a-size-base a-color-secondary a-text-normal">Envíos desde y vendidos por Amazon.</span>
+                                    </div>
+                                    <div className="a-row">
+                                      <span className="a-size-base a-color-secondary a-text-normal">
+                                        Está disponible el
+                                        <b>envío GRATIS</b>
+                                        para este producto.
+                                        <a className="a-link-normal sims-fbt-details-help-link a-declarative" target="AmazonHelp" rel="noopener" href="https://www.amazon.es/gp/help/customer/display.html/ref=pd_bxgy_3_sdc_sdc_3/259-6544150-3296161?ie=UTF8&amp;nodeId=201911090&amp;pd_rd_r=c6b0e583-be80-4aa3-867d-b89df4127853&amp;pd_rd_w=yDUK0&amp;pd_rd_wg=nVWqQ&amp;pf_rd_p=12d945b9-5148-4c92-8c2a-6d4b36ee4de4&amp;pf_rd_r=VBVEMCD60R9FPEJFGNHW&amp;pop-up=1&amp;refRID=VBVEMCD60R9FPEJFGNHW">Detalles</a>
+                                      </span>
+                                    </div>
+                                  </div>
+                                </div>
+                              </span>
+                            </label>
+                          </div>
+                        </span>
+                      </span>
+                    </li>
+                  </ul>
+                </fieldset>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default BoughtTogether;
